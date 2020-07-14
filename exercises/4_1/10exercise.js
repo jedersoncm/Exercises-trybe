194ea6cdf0f66f88741e9a00520bfca9d7f30ee1
jederson <jmacedo.ti@gmail.com>
2020-07-14 11:36:52 -0300

    Ajustando ex 10

--- exercises/4_1/10exercise.js
+++ exercises/4_1/10exercise.js
@@ -1,8 +1,8 @@
-let custoProd = 2;
+let custoProd = 0;
 let valorVend = 3.4;
 let lucro = 0;
 
-if (custoProd >= 0 || valorVend > 0){
+if (custoProd > 0 && valorVend > 0){
   lucro  = (valorVend - (custoProd * 1.2)) * 1000;
   console.log(lucro);
 }
