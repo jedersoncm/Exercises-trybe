d0a8cd81f201ae6ce48181046bb97381b9c44aed
jederson <jmacedo.ti@gmail.com>
2020-08-10 15:47:49 -0300

    exercise 01

---
+++ exercises/7_1/exercise01.js
@@ -0,0 +1,7 @@
+ const fact = (num) => num > 1 ?  num * fact(num -1): 1
+
+ console.log(fact(6));
+
+// const factorial = number => number > 1 ? number * factorial(number - 1) : 1
+
+// console.log(factorial(5))
